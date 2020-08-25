0148a841273a9c5f11a73490aab8124adca16a9e
Parminderk766 <51168168+Parminderk766@users.noreply.github.com>
2020-08-25 13:18:31 -0400

    Items done

---
+++ models/items.js
@@ -0,0 +1,23 @@
+Const db = require('./db');
+const {Sequelize} = require('sequelize');
+
+const Items = db.define('cars', {
+    name: {
+        type: Sequelize.STRING,
+        allowNULL: false,
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNULL:false,
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    imageUrl:  {
+        type: Sequelize.STRING,
+        allowNULL: false,
+    }
+})
+
+module.exports = Items;
