0e21fefae5c01058fd4c8d6dc5c4aa65359ba347
MikaJen <ttwiggs224@gmail.com>
2020-08-25 12:23:12 -0500

    commit fix to branch

--- client/src/Students.js
+++ client/src/Students.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 
-export default function func() {
+export default function Students() {
     const [state, setState] = useState({
         students: [],
         loading: true
