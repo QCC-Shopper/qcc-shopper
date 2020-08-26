7fcd7270a146b489ac5be3bfd4129a70b905b354
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-26 07:42:35 -0400

    removing db.js from merge

--- models/db.js
+++
@@ -1,8 +0,0 @@
-// link to database goes here
-const Sequelize = require('sequelize')
-const db = new Sequelize(
-    'postgres://aidanfarhi:secret123@localhost:5432/qccshopper', 
-    {logging: false},
-)
-
-module.exports = db
