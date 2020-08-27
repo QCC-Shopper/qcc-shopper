e1d03ea6acd39efa266f76d6cda4779b3a8dd9fa
Parminderk766 <51168168+Parminderk766@users.noreply.github.com>
2020-08-27 12:25:51 -0400

    l

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,25 +1,34 @@
 import React, {Component} from 'react';
 
 // props need change handler funcions for the form and the three buttons
-const NavBar = (props) => {
-    return(
-        <div className='navbar'>
-            <h1>LeetRide</h1>
+class NavBar extends component{
+    constructor(props){
+        super(props);
+        this.state = {
 
-            <form className='searh-form'>
-                <input type='text' placeholder='Find your ride'/>
-                <button type='submit'>Search</button>
-            </form>
+        }
+    }
 
-            <div className='btn-navbar'>
-                <button>Browse Cars</button>
-                <button>Cart</button>
-                <button>Sign In</button>
+
+    render() {
+        return(
+            <div className='navbar'>
+                <h1>LeetRide</h1>
+    
+                <form className='searh-form'>
+                    <input type='text' placeholder='Find your ride'/>
+                    <button type='submit'>Search</button>
+                </form>
+    
+                <div className='btn-navbar'>
+                    <button><link>Browse Cars</link></button>
+                    <button>Cart</button>
+                    <button>Sign In</button>
+                </div>
             </div>
-            
-
-        </div>
-    )
+        )
+    }
 }
+    
 
 export default NavBar;
