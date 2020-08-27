4600931093d0246eb932e4b7e39eff8bfb67f97a
Parminderk766 <51168168+Parminderk766@users.noreply.github.com>
2020-08-27 11:42:32 -0400

    created navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import Students from './Students'
-import './App.css';
+import React, {Component} from 'react';
+import Students from './Students';
+import NavBar from './NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Students />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      item: []
+    }
+  }
+
+  
+  render(){
+  return(
+    <div className='container'>
+      <NavBar/>
     </div>
-  );
+  )
 }
+}
+
 
 export default App;
