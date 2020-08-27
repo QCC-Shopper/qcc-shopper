8ef22a30d7faf45da9267939f165985b65014d15
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2020-08-27 14:25:36 -0400

    added AllUsers Component. Currently rendering a list of Links. Need to establish a path to the Single User Component.

---
+++ client/src/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+// Single user imports Here
+// Ex: import User from './components/User
+
+class Routes extends React.Component {
+
+    render() {
+        return (
+            <Switch>
+                {/* <Route path='/user' component={User}/> */}
+            </Switch>
+        )
+    }
+}
+
+export default Routes
---
+++ client/src/components/AllUsers.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default class AllUsers extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            users: []
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            const response = await fetch('/user')
+            const data = await response.json()
+            const userLinks = data.map((user, i) => <Link key={i} to={{pathname:'/test', query:{id: user.id.toString()}}}>{user.name}</Link>)                                      
+            this.setState({
+                users: userLinks
+            })
+        } catch(er) {console.log(er)}
+    }
+
+    render() {
+        return (
+            <div className='users-div-main'>
+                    {this.state.users}
+            </div>
+        )
+    }
+}
