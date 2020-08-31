6ffc73ce9fc7c891d90185f28c4a355ee72558a0
MikaJen <ttwiggs224@gmail.com>
2020-08-31 17:46:20 -0500

    allItems

--- client/src/Components/AllItems.js
+++ client/src/Components/AllItems.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component, useReducer } from 'react'
+import {Link} from 'react-router-dom'
 
+export default class AllItems extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            items:[]
+        }
+    }
+}
 
-
+async componentDidMount() {
+    try {
+        const response = await fetch('/items')
+        const data =await response.json()
+        const itemLinks = data.map((items, i)=> <Link key={i} to={{pathname:'/test', query:{id: useReducer.id.toString()}}}>(items.name)</Link>)
+        this.ListeningStateChangedEvent({
+            items: itemLinks
+        })
+    }  catch(er) {console.log(er)}
+}
 
 class AllItems extends Component {
     render() {
         return (
-        <div className=""> 
+        <div className="items-div-main"> 
+                    {this.state.items}
             <header className= "">
                 <h1 className="App-title"> All Items</h1>
             </header>
