18ad263aae5dec3651bfd4b9c25a606843607535
MikaJen <ttwiggs224@gmail.com>
2020-09-01 18:09:13 -0500

    delete

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import AllUsers from './components/AllUsers'
 import Routes from './Routes'
 import './App.css';
-
-import Items from "./Components/AllItems";
+//import AllItems from './Components/AllItems';
+// import Items from "./Components/Items";
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-<<<<<<< HEAD
-        <Items />
+        {/* <Items /> */}
+        {/* <AllItems /> */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -24,10 +24,8 @@ function App() {
         >
           Learn React
         </a>
-=======
         <AllUsers />
         <Routes />
->>>>>>> fbe8345cab4e1f438b958f851c252644ab759dce
       </header>
     </div>
   );
--- client/src/Components/AllItems.js
+++ client/src/Components/AllItems.js
@@ -1,38 +1,47 @@
-import React, { Component, useReducer } from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-export default class AllItems extends React.Component {
+
+const {items} = [
+    {id: 1, make: 'a', model: 'a1', price:'$ a.aa' },
+    {id: 3, make: 'b', model: 'b1', price:'$b.bb ' },
+    {id: 2, make: 'c', model: 'c1', price:'$ c.cc' }
+
+]; 
+
+class AllItems extends React.Component {
     constructor() {
         super()
         this.state = {
             items:[]
-        }
-    }
-}
-/*
+    };
+}       
+   
 
-async componentDidMount () {
-    try {
-        const response = await fetch('/items')
+
+async showItems(){
+    console.log(AllItems);
+        const response = await fetch('./items')
         const data =await response.json()
-        const itemLinks = data.map((items, i)=> <Link key={i} to={{pathname:'/test', query:{id: useReducer.id.toString()}}}>(items.name)</Link>)
+        // const itemLinks = data.map((items, i)=> <Link key={i} to={{pathname:'/test', query:{id: useReducer.id.toString()}}}>(items.name)</Link>)
+        const itemLinks= data
         this.ListeningStateChangedEvent({
             items: itemLinks
-        })
-    }  catch(er) {console.log(er)}
+        }
+    )
+    
 }
-*/
-class AllItems extends Component {
     render() {
-        return (
+    return (
         <div className="items-div-main"> 
-                    {this.state.items}
+                <span className={this.state.items()}>Item List</span>
             <header className= "">
                 <h1 className="App-title"> All Items</h1>
             </header>
+        <ul>Item1</ul>
+        <ul>Item2</ul>
         </div>
         );
     }
 }
-
-export default AllItems;
+// export default AllItems;
