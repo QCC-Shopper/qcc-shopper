bf21e4212f135fa9facd38b5d8ec0f645420b363
Parminderk766 <51168168+Parminderk766@users.noreply.github.com>
2020-09-02 10:37:07 -0400

    Routes.js and index.js

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,12 +2,21 @@ import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 // Single user imports Here
 import SingleUser from './components/SingleUser.js'
+// NavBar imports here
+import NavBar from './components/NavBar.js'
+// AllUsers imports here
+import AllUsers from './components/AllUsers.js'
+// AllItems imports here
+import AllItems from './components/AllItems.js'
 
 class Routes extends React.Component {
     render() {
         return (
             <Switch>
                 <Route path='/user' component={SingleUser}/>
+                <Route path='/NavBar' component={NavBar}/>
+                <Route path='/AllUsers' component={AllUsers}/>
+                <Route path='/AllItems' component={AllItems}/>
             </Switch>
         )
     }
