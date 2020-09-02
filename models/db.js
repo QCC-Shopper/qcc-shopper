e8f6e52a0834176d1884fa8052414663d6b65e19
ayamkito <38877140+ayamkito@users.noreply.github.com>
2020-09-02 17:55:37 -0400

    Delete db.js

--- models/db.js
+++
@@ -1,8 +0,0 @@
-
-// link to database goes here
-const Sequelize = require('sequelize')
-const db = new Sequelize(
-    'postgres://kakwo:password@localhost:5432/shopper', 
-    {logging: false},
-)
-module.exports = db
