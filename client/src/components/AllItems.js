9794eae2884757171160731861f1016ec0d25cab
Parminderk766 <51168168+Parminderk766@users.noreply.github.com>
2020-09-03 11:35:45 -0400

    small fixes in AllItems.js and Index.css and added some commets to clarify things in the code

--- client/src/components/AllItems.js
+++ client/src/components/AllItems.js
@@ -23,7 +23,7 @@ render() {
         <div className='car-images-container'>
             {this.state.items.map((item, i)=> <div className="car-images">
                 <img src={item.imageUrl}></img>
-                    <p><Link key={i} to={{pathname:'/test'}}>{item.name}</Link></p>
+                    <h3><Link key={i} to={{pathname:'/test'}}>{item.name}</Link></h3>
                     <p>Price: {item.price}</p>
             </div>)}
         </div>
